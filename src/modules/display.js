68746d5d061df4d4f1cac73a2fbf0cfdab79ebd6
lisandroseia <lisandroseia11@gmail.com>
2022-01-26 19:32:03 -0300

    Add api to  work

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,18 +1,13 @@
 import getNode from './getNode.js';
 
 const board = document.querySelector('.board');
-const name = document.getElementById('name');
-const score = document.getElementById('score');
 
-const display = () => {
-  if (name.value === '' || score.value === '') {
-    return;
+const display = (name,score) => {
+  if(name === undefined || score === undefined){
+    return
   }
-  if (parseInt(score.value, 10)) {
-    board.appendChild(getNode(name.value, score.value));
-    name.value = '';
-    score.value = '';
-  }
+  
+   board.appendChild(getNode(name, score));
 };
 
 export default display;
